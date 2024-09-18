a161e6f7a3603eb4e93fda4f3f7951c2366599b9
gustavokra <gustavo.kraemer6@gmail.com>
2024-09-18 13:18:05 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Router from './components/router/Router';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import Dashboard from './pages/DashBoard/Dashboard';
+import Sobre from './pages/Sobre/Sobre';
 
-function App() {
-  return (
+
+
+function App() { 
+  const renderizarBotoes = () => (
     <div className="App">
-      <Router></Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Login</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+        </ul>
+      </nav>
     </div>
+  );
+
+  return (
+        <Router>
+          {renderizarBotoes()}
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+          </Routes>
+        </Router>
   );
 }
 
