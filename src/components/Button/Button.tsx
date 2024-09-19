3add949a2fede2c73611207d9c493ae852201a0e
gustavokra <gustavo.kraemer6@gmail.com>
2024-09-19 17:35:01 -0300

    commit para entrega

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import "./Button.css"
+
+interface IButtonProps {
+    text: string;
+    secondary?: boolean;
+    onClick?: () => void;
+}
+
+export default function Button({ text, secondary, onClick }: IButtonProps) {
+    return (
+        <button
+            className={secondary ? "btn-secondary" : "btn-primary"}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
